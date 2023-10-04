4006ed8079c2806a91b401605a9d1cd93d87fdb4
somchai sik <somchai7691@gmail.com>
2023-10-03 22:56:57 -0700

    elder upadted

--- src/components/elderly/Profiles.jsx
+++ src/components/elderly/Profiles.jsx
@@ -1,61 +1,80 @@
-import React, { useState } from 'react';
-
-import axios from '../../services/axios';
+import React, { useState, useEffect } from "react";
+import axios from "../../services/axios";
+import "./Profiles.css";
+import { Alert, Select, Form, Input, DatePicker, TimePicker } from "antd";
+import { Button } from "antd";
 
 const Profiles = () => {
   const [formData, setFormData] = useState({
-    name: '',
-    lName: '',
-    age: '',
-    gender: '',
-    contactNumber: '',
-    interest: '',
-    eContact: '',
+    name: "",
+    lName: "",
+    age: "",
+    gender: "male",
+    contactNumber: "",
+    interest: "",
+    emergencyContact: "",
   });
+
+  const [profile, setProfile] = useState({});
+
+  const fetchUserProfile = async () => {
+    try {
+      const getProfile = await axios.getRequest("user", true);
+      setProfile(getProfile); // Set the user profile data in the state
+    } catch (error) {
+      console.error("Error fetching user profile:", error);
+    }
+  };
+
+  useEffect(() => {
+    // Fetch user profile data when the component mounts
+    fetchUserProfile();
+  }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      const response = await axios.postRequest('updateProfile', formData, true);
+      const response = await axios.postRequest("updateProfile", formData, true);
 
       // Handle success, e.g., show a success message to the user
-      console.log('Form submission successful:', response.data);
-      console.log(response)
+
+      console.log("Form submission successful:", response.data);
+      console.log(response);
 
       // Optionally, you can reset the form fields
       setFormData({
-        name: '',
-        lName: '',
-        age: '',
-        gender: '',
-        contactNumber: '',
-        interest: '',
-        eContact: '',
+        name: "",
+        lName: "",
+        age: "",
+        gender: "",
+        contactNumber: "",
+        interest: "",
+        emergencyContact: "",
       });
     } catch (error) {
-      
-      console.error('Form submission error:', error);
+      console.error("Form submission error:", error);
     }
-    console.log(formData)
+    console.log(formData);
+    console.log(profile);
+
+    fetchUserProfile();
   };
 
-  
   const handleInputChange = (e) => {
     const { name, value } = e.target;
+
     setFormData({
       ...formData,
       [name]: value,
     });
-    
+    console.log(formData);
   };
 
-
-  
   return (
     <div>
-      <h1>Elderly Profile</h1>
-      <form onSubmit={handleSubmit}>
+      {/* <h1>Elderly Profile</h1>
+      <form className="elderFrom" onSubmit={handleSubmit}>
         <label htmlFor="name">Name</label>
         <input
           id="name"
@@ -64,8 +83,6 @@ const Profiles = () => {
           value={formData.name}
           onChange={handleInputChange}
         />
-
-     
 
         <label htmlFor="age">Age</label>
         <input
@@ -106,10 +123,94 @@ const Profiles = () => {
           onChange={handleInputChange}
         />
 
-    
+        <label htmlFor="emergencyContact">Emergency Contact</label>
+        <input
+          id="emergencyContact"
+          type="number"
+          name="emergencyContact"
+          value={formData.emergencyContact}
+          onChange={handleInputChange}
+        />
 
         <input type="submit" value="Submit" />
-      </form>
+      </form> */}
+
+      <div className="displayProfile">
+        <h2>User Profile</h2>
+        <p>Name: {profile.name}</p>
+        <p>Age: {profile.age}</p>
+        <p>Gender: {profile.gender}</p>
+        <p>Contact Number: {profile.contactNumber}</p>
+        <p>Interest: {profile.interest}</p>
+        <p>Emergency Contact: {profile.emergencyContact}</p>
+      </div>
+
+      <Form
+        name="trigger"
+        style={{
+          maxWidth: 400,
+        }}
+        layout="vertical"
+        autoComplete="off"
+      >
+        <Form.Item label="Name">
+          <Input
+            placeholder="Service title"
+            name="name"
+            value={formData.name}
+            onChange={handleInputChange}
+          />
+        </Form.Item>
+        <Form.Item label="Age">
+          <Input
+            placeholder="Service type"
+            name="age"
+            value={formData.age}
+            onChange={handleInputChange}
+          />
+        </Form.Item>
+        <Form.Item label="Contact Number">
+          <Input
+            placeholder="Service type"
+            name="contactNumber"
+            value={formData.contactNumber}
+            onChange={handleInputChange}
+          />
+        </Form.Item>
+        <Form.Item label="interest">
+          <Input
+            placeholder="Service type"
+            name="interest"
+            value={formData.interest}
+            onChange={handleInputChange}
+          />
+        </Form.Item>
+
+        <label htmlFor="gender">Gender</label>
+        <select
+          id="gender"
+          name="gender"
+          value={formData.gender}
+          onChange={handleInputChange}
+        >
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+          <option value="prefered">Prefer not to say</option>
+        </select>
+
+        <Form.Item label="Emergency Contact">
+          <Input
+            placeholder="Service type"
+            name="emergencyContact"
+            value={formData.emergencyContact}
+            onChange={handleInputChange}
+          />
+        </Form.Item>
+
+        <Button type="primary" onClick={handleSubmit}>
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 };
