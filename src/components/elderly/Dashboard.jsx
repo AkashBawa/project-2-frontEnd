12ddea12100fbc4a2045e9d31a17780a19a81af6
Ahmed Abdou <hakam4all@yahoo.com>
2023-10-14 23:35:54 -0700

    modify dashboard

--- src/components/elderly/Dashboard.jsx
+++ src/components/elderly/Dashboard.jsx
@@ -2,8 +2,16 @@ import { Outlet } from "react-router-dom"
 
 const Dashboard = () => {
   return (
-    <div>
-        Dashboard Elder
+    <div className="dashBoardElder">
+      <h1>Elder Dashboard</h1>
+      <div className="dashElder">
+        <div className="dashElderEvent">Upcoming Event</div>
+        <div className="dashElderPending">Pending Approval</div>
+        <div className="dashElderOpen">Open Request</div>
+      </div>
+      <div className="postCard">
+        <div></div>
+      </div>
     </div>
   )
 }
