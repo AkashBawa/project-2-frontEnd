42c3731c39d89bffc6e9c118901df1934f30bac9
Mahima Mukhi <mmukhi00@mylangara.ca>
2023-10-21 12:03:19 -0700

    add map

--- src/components/elderly/addPost.jsx
+++ src/components/elderly/addPost.jsx
@@ -1,10 +1,10 @@
 import { Alert, Form, Input, DatePicker, TimePicker } from "antd";
 import { useState } from "react";
-import { Button } from 'antd';
+import { Button } from "antd";
 import axios from "../../services/axios";
+import TomTomAutoComplete from "../map/TomTomAutoComplete";
 
 const AddPost = () => {
-
   const [time, setTime] = useState();
   const [serviceTitle, setserviceTitle] = useState();
   const [serviceType, setserviceType] = useState();
@@ -18,16 +18,16 @@ const AddPost = () => {
       time,
       serviceTitle,
       serviceType,
-      "location": {
-        "coordinates": [ -122.778620, 49.163640]
-      } 
-    }
+      location: {
+        coordinates: [-122.77862, 49.16364]
+      }
+    };
 
     if (time && serviceTitle && serviceType && date) {
       const response = await axios.postRequest("addpost", payload, true);
-      console.log(response)
+      console.log(response);
     }
-  }
+  };
   return (
     <div>
       <h2>Add post</h2>
@@ -35,32 +35,57 @@ const AddPost = () => {
         <Form
           name="trigger"
           style={{
-            maxWidth: 400,
+            maxWidth: 400
           }}
           layout="vertical"
           autoComplete="off"
         >
           {/* <Alert message="Use 'max' rule, continue type chars to see it" /> */}
-
           <Form.Item label="Service title">
-            <Input placeholder="Service title" onChange={(e) => {setserviceTitle(e.target.value)}} />
+            <Input
+              placeholder="Service title"
+              onChange={e => {
+                setserviceTitle(e.target.value);
+              }}
+            />
           </Form.Item>
-          <Form.Item label="Service type" >
-            <Input placeholder="Service type" onChange={(e) => {setserviceType(e.target.value)}}  />
+          <Form.Item label="Service type">
+            <Input
+              placeholder="Service type"
+              onChange={e => {
+                setserviceType(e.target.value);
+              }}
+            />
           </Form.Item>
           <Form.Item label="Date">
-            <input type="date" onChange={(e) => {setdate(e.target.value)}} />
+            <input
+              type="date"
+              onChange={e => {
+                setdate(e.target.value);
+              }}
+            />
           </Form.Item>
           <Form.Item label="Select time">
-            <input type="time" onChange={(e) => {setTime(e.target.value)}} required />
+            <input
+              type="time"
+              onChange={e => {
+                setTime(e.target.value);
+              }}
+              required
+            />
           </Form.Item>
 
-          <Button type="primary" onClick={submit}>Submit</Button>
-        </Form>
+          {/* map */}
 
+          <TomTomAutoComplete />
+
+          <Button type="primary" onClick={submit}>
+            Submit
+          </Button>
+        </Form>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AddPost
+export default AddPost;
---
+++ src/components/map/TomTomAutoComplete.jsx
@@ -0,0 +1,153 @@
+import React, { useState, useEffect, useRef } from "react";
+import tt from "@tomtom-international/web-sdk-maps";
+
+const TomTomAutoComplete = () => {
+  const [query, setQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [userLocation, setUserLocation] = useState(null); // User's location
+  const [selectedSuggestion, setSelectedSuggestion] = useState(null);
+  const [map, setMap] = useState({});
+  const [marker, setMarker] = useState(null);
+
+  const apiKey = "JwHnAZhAnIimgNZBeSVdqbeElmbtyAh5"; // Replace with your TomTom API key
+
+  const mapElement = useRef();
+
+  // Function to get the user's location using the Geolocation API
+  const getUserLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        const { latitude, longitude } = position.coords;
+        setUserLocation({ lat: latitude, lon: longitude });
+      });
+    }
+  };
+
+  // Fetch suggestions based on user location
+  useEffect(() => {
+    if (userLocation && query.length > 2) {
+      fetch(
+        `https://api.tomtom.com/search/2/search/${encodeURIComponent(
+          query
+        )}.json?key=${apiKey}&lat=${userLocation.lat}&lon=${userLocation.lon}`
+      )
+        .then(response => response.json())
+        .then(data => {
+          if (data.results) {
+            setSuggestions(data.results);
+          }
+        })
+        .catch(error => {
+          console.error("Error fetching suggestions: ", error);
+        });
+    } else {
+      setSuggestions([]);
+    }
+  }, [userLocation, query]);
+
+  const handleInputChange = event => {
+    const inputValue = event.target.value;
+    setQuery(inputValue);
+    setSelectedSuggestion(null); // Reset the selected suggestion
+  };
+
+  const handleSuggestionSelect = suggestion => {
+    setQuery(suggestion.address.freeformAddress);
+    setSuggestions([]); // Clear the suggestions list
+    setSelectedSuggestion(suggestion);
+  };
+
+  // Get the user's location when the component mounts
+  useEffect(() => {
+    getUserLocation();
+  }, [selectedSuggestion]);
+
+  // Default Value
+  useEffect(() => {
+    let map = tt.map({
+      key: apiKey,
+      container: mapElement.current,
+      center: ["-123.138572", "49.263566"],
+      zoom: 12
+    });
+    const newMarker = new tt.Marker()
+      .setLngLat(["-123.138572", "49.263566"])
+      .addTo(map);
+
+    setMap(map);
+    setMarker(newMarker);
+
+    return () => {
+      map.remove();
+      newMarker.remove();
+    };
+  }, []);
+
+  useEffect(() => {
+    // console.log("selectedSuggestion", selectedSuggestion.position)
+    if (selectedSuggestion) {
+      let map = tt.map({
+        key: apiKey,
+        container: mapElement.current,
+        center: [
+          selectedSuggestion.position.lon,
+          selectedSuggestion.position.lat
+        ],
+        zoom: 12
+      });
+
+      const newMarker = new tt.Marker()
+        .setLngLat([
+          selectedSuggestion.position.lon,
+          selectedSuggestion.position.lat
+        ])
+        .addTo(map);
+
+      setMap(map);
+      setMarker(newMarker);
+
+      return () => {
+        map.remove();
+        newMarker.remove();
+      };
+    }
+  }, [selectedSuggestion]);
+
+  console.log(
+    "selectedSuggestion",
+    selectedSuggestion == null
+      ? {
+          lat: -123.138572,
+          lon: 49.263566
+        }
+      : selectedSuggestion.position
+  );
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Enter an address"
+        value={query}
+        onChange={handleInputChange}
+      />
+      <ul>
+        {suggestions.map(suggestion => (
+          <li
+            key={suggestion.id}
+            onClick={() => handleSuggestionSelect(suggestion)}
+          >
+            {suggestion.address.freeformAddress}
+          </li>
+        ))}
+      </ul>
+      {selectedSuggestion && (
+        <p>Selected: {selectedSuggestion.address.freeformAddress}</p>
+      )}
+
+      <div ref={mapElement} className="mapDiv"></div>
+    </div>
+  );
+};
+
+export default TomTomAutoComplete;
