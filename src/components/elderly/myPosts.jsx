5f7126cb65778894cc719b26e82021e89e2f9c69
AkashBawa <bawa2027@gmail.com>
2023-11-11 21:38:21 -0800

    [dashboard and loaders]

--- src/components/elderly/myPosts.jsx
+++ src/components/elderly/myPosts.jsx
@@ -9,88 +9,73 @@ import Edit from './../../images/edit.png';
 import moment from "moment";
 
 
-const MyPosts = () => {
+const MyPosts = ({ posts }) => {
 
-  const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    fetchMyPosts();
+    // fetchMyPosts();
   }, []);
 
-  const fetchMyPosts = async () => {
-    try {
-      const response = await axios.getRequest("getPostByUser", true);
-      if (response.success === true) {
-        setPosts(response.posts);
-      }
-    } catch (err) {
-      console.log(err)
-    }
-  }
-
-  const findBookedIndex = (post) => {
-    const index = post?.invitations.map((invite) => { return invite.status }).indexOf("ACCEPTED");
-    return index;
-  }
-
-  const responseInvitation = async (postIndex, acceptedUserId, status) => {
-    try {
-      const postId = posts[postIndex]._id;
-      const response = await axios.putRequest("responseInvitation", { postId, acceptedUserId, status }, true);
-      // console.log("This is the post content From MyPost" + response);
-      fetchMyPosts();
-
-    } catch (err) {
-      console.log(err)
-    }
-  }
+  // const fetchMyPosts = async () => {
+  //   try {
+  //     const response = await axios.getRequest("getPostByUser", true);
+  //     if (response.success === true) {
+  //       setPosts(response.posts);
+  //     }
+  //   } catch (err) {
+  //     console.log(err)
+  //   }
+  // }
 
   return (
     <div >
-      <div id="postTableHeader">
+      {/* <div id="postTableHeader">
         <h3>All Posts()</h3>
         <h3>Active Posts()</h3>
         <h3>History()</h3>
-      </div>
+      </div> */}
 
       {
         posts.map((post, postIndex) => {
           // console.log(post.invitations[0].user.userName);
-          console.log(post);
+          // console.log(post);
 
           return (
 
             <Card
               key={`card-${postIndex}`}
               id="myPostsCard"
-            // title={post.invitations[0].user.userName ? `${post.invitations[0].user.userName}` : `User Name Not Found`}
-            // extra={post.status}
             >
               <div className="cardBody">
-                {
-                  <img className="eventImage" src={DeleteImage} alt="Event Image" />
-                }
-                {
-                  <div className="eventDetails">
-                    <h3>{post.serviceTitle}</h3>
-                    <p>location</p>
-                    <div className="myPostDT">
-                      {moment(post.date).format("MMM DD, YYYY")} {moment(post.time, "HH:mm").format("hh A")}
 
-                    </div>
-                  </div>
-                }
-                {
-                  <div className="deleteEditSection">
-                    <div className="myPostDelete">
-                      <Link to='/elder/profile'><img src={DeleteImage} alt="DeleteImage" /></Link>
-                    </div>
+                <img className="eventImage" src={DeleteImage} alt="Event Image" />
 
-                    <div className="myPostEdit">
-                      <img src={Edit} alt="Edit" />
-                    </div>
+                <div className="eventDetails">
+                  <h3>{post.serviceTitle}</h3>
+                  <p>location</p>
+                  <div className="myPostDT">
+                    {moment(post.date).format("MMM DD, YYYY")} {moment(post.time, "HH:mm").format("hh A")}
+
                   </div>
+                </div>
+
+                {
+                  post.status == "PENDING" && <>
+                    <div className="deleteEditSection">
+                      <div className="myPostDelete">
+                        <Link to='/elder/profile'><img src={DeleteImage} alt="DeleteImage" /></Link>
+                      </div>
+
+                      <div className="myPostEdit">
+                        <img src={Edit} alt="Edit" />
+                      </div>
+                    </div>
+                  </>
+
                 }
+
+
+
               </div>
 
             </Card>
