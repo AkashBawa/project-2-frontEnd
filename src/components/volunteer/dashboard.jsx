42c80ffe1b57812a5cc018042961eeea1c6e5b34
Ahmed Abdou <hakam4all@yahoo.com>
2023-11-18 09:26:23 -0800

    volunteer dashboard 18-Nov

--- src/components/volunteer/dashboard.jsx
+++ src/components/volunteer/dashboard.jsx
@@ -2,7 +2,7 @@ import { Card, Space, Button } from "antd";
 import { Outlet, Link } from "react-router-dom"
 import { Radio, Tabs } from 'antd';
 
-import "./dashboard.css"
+import "./../volunteer/css/dashboard.css"
 import { useState } from "react";
 import MyPosts from './../elderly/myPosts';
 import SinglePostView from "./../elderly/viewSinglePost";
@@ -15,6 +15,8 @@ import iconProfile from './../../images/icon_profile.png';
 import iconNotification from './../../images/icon_notification.png';
 import iconNavProfile from './../../images/icon_profile_mobile.png';
 import iconNavNotification from './../../images/icon_request_mobile.png';
+import rewards from './../../images/rewards.png';
+
 import { useDispatch } from "react-redux";
 import { setLoader } from '../../redux/user';
 
@@ -78,7 +80,7 @@ const Dashboard = () => {
     }
   }
 
-  
+
   const fetchMyPosts = async () => {
     try {
       setSingleView(false);
@@ -102,122 +104,121 @@ const Dashboard = () => {
 
   return (
     <>
-    {
-      singleView == false && (
-        <div className="dashBoardElder">
-          <nav>
-            <img src={statusBar} alt="statusBar" id="statusBar" />
-            <div>
-              <img src={wiseCareLogo} alt="Logo" />
-              <div className="navtopIcons">
-                <img src={iconNavNotification} alt="iconNavNotification" />
-                <Link to='/elder/profile'><img src={iconNavProfile} alt="iconNavProfile" /></Link>
+      {
+        singleView == false && (
+          <div className="dashBoardVolunteer">
+            <nav>
+              <img src={statusBar} alt="statusBar" id="statusBar" />
+              <div>
+                <img src={wiseCareLogo} alt="Logo" />
+                <div className="navtopIcons">
+                  <img src={iconNavNotification} alt="iconNavNotification" />
+                  <Link to='/elder/profile'><img src={iconNavProfile} alt="iconNavProfile" /></Link>
+                </div>
+              </div>
+            </nav>
+            <div className="dashBoardVolunteerHeader">
+              <h1>Hi, Beant</h1>
+              <div className="topIconsVolunteer">
+                <img src={iconNotification} alt="iconNotification" />
+                <Link to='/elder/profile'><img src={iconProfile} alt="iconProfile" /></Link>
               </div>
             </div>
-          </nav>
-          <div className="dashBoardElderHeader">
-            <h1>Hi, Ana</h1>
-            <div className="topIcons">
-              <img src={iconNotification} alt="iconNotification" />
-              <Link to='/elder/profile'><img src={iconProfile} alt="iconProfile" /></Link>
+            <div className="dashVolunteerNav">
+              <div className="dashVolunteerEvent">
+                <h2>All Tasks</h2>
+                <div className="allTasksCount">{approvedCounter}</div>
+              </div>
+              <div className="dashVolunteerUnanswered">
+                <h3>Active Tasks</h3>
+                <div className="activeTasks">{approvedCounter}</div>
+              </div>
+              <div className="dashVolunteerPending">
+                <h3>Rewards</h3>
+                <img src={rewards} alt="Rewards" />
+                <h4>Next Medal:  6 / 15</h4>
+              </div>
+            </div>
+
+            <div className="deletePostConfirmation Visually-hidden ">
+              <div>
+                <h3>Are you sure you want to delete you post?</h3>
+                <button className="deleteNo">No</button>
+                <button className="deleteYes">Yes</button>
+              </div>
+            </div>
+
+            <div id="postsSection">
+              <div id="postsSectionNav">
+                <h1>My Tasks</h1>
+              </div>
+
+              <Tabs className="tabs"
+                defaultActiveKey="1"
+                type="card"
+                size={"middle"}
+                items={
+                  [
+                    {
+                      label: `All Posts(${pendingCounter})`,
+                      key: "1",
+                      children: <MyPosts posts={pendingPosts} changeSingleView={changeSingleView} fetchMyPosts={fetchMyPosts} />,
+                    },
+                    {
+                      label: `Active Posts(${approvedCounter})`,
+                      key: "2",
+                      children: <MyPosts posts={approvedPosts} changeSingleView={changeSingleView} fetchMyPosts={fetchMyPosts} />,
+                    },
+                    {
+                      label: `History(${completedCounter})`,
+                      key: "3",
+                      children: <MyPosts posts={completedPosts} changeSingleView={changeSingleView} fetchMyPosts={fetchMyPosts} />,
+                    },
+                  ]
+                }
+              />
+
             </div>
           </div>
-          <div className="dashElderNav">
-            <div className="dashElderEvent">
-              <h2>Join our Events</h2>
-              <h4>Join us for our upcoming session.</h4>
-              <button className="eventMore">Join</button>
-            </div>
-            <div className="dashElderUnanswered">
-              <h3>Active Posts</h3>
-              <div className="unansCount">{approvedCounter}</div>
-            </div>
-            <div className="dashElderPending">
-              <h3>All Posts</h3>
-              <div className="pendingCount">{pendingCounter}</div>
-            </div>
+        )
+      }
+
+      {
+        singleView == true && (
+          <div>
+            <SinglePostView currentPost={currentPost} fetchMyPosts={fetchMyPosts} />
           </div>
+        )
+      }
+      <div className="list-posts">
+        <Space direction="vertical">
 
-          <div className="deletePostConfirmation Visually-hidden ">
-            <div>
-              <h3>Are you sure you want to delete you post?</h3>
-              <button className="deleteNo">No</button>
-              <button className="deleteYes">Yes</button>
-            </div>
-          </div>
+          {
+            posts.map((post, index) => {
+              return (
+                <Card
+                  key={`card-${index}`}
+                  title={post.serviceTitle}
+                  extra={
+                    post.invitations.map((invite) => invite.user).indexOf(userId) > -1 ?
+                      <Button type="default" disabled>Already send</Button> :
+                      <Button type="default" onClick={() => { sendRequest(post._id, index) }}>Send Request</Button>
+                  }
+                  style={{
+                    width: "100%",
+                  }}
 
-          <div id="postsSection">
-            <div id="postsSectionNav">
-              <h1>My Posts</h1>
-              <button id="createPost"><Link to='/elder/addPost'>Create Post</Link></button>
-            </div>
-
-            <Tabs className="tabs"
-              defaultActiveKey="1"
-              type="card"
-              size={"middle"}
-              items={
-                [
-                  {
-                    label: `All Posts(${pendingCounter})`,
-                    key: "1",
-                    children: <MyPosts posts={pendingPosts} changeSingleView={changeSingleView}  fetchMyPosts={fetchMyPosts} />,
-                  },
-                  {
-                    label: `Active Posts(${approvedCounter})`,
-                    key: "2",
-                    children: <MyPosts posts={approvedPosts} changeSingleView={changeSingleView} fetchMyPosts={fetchMyPosts} />,
-                  },
-                  {
-                    label: `History(${completedCounter})`,
-                    key: "3",
-                    children: <MyPosts posts={completedPosts} changeSingleView={changeSingleView} fetchMyPosts={fetchMyPosts} />,
-                  },
-                ]
-              }
-            />
-
-          </div>
-        </div>
-      )
-    }
-
-    {
-      singleView == true && (
-        <div>
-          <SinglePostView currentPost={currentPost} fetchMyPosts={fetchMyPosts} />
-        </div>
-      )
-    }
-    <div className="list-posts">
-      <Space direction="vertical">
-
-       {
-        posts.map((post, index) => {
-          return (
-            <Card
-            key={`card-${index}`}
-            title= {post.serviceTitle}
-            extra={
-              post.invitations.map((invite) => invite.user).indexOf(userId) > -1 ? 
-              <Button type="default" disabled>Already send</Button> : 
-              <Button type="default" onClick={() => {sendRequest(post._id, index)}}>Send Request</Button>
-            }
-            style={{
-              width: "100%",
-            }}
-
-          >
-            <p>Description</p>
-            <p>By: {post?.userId.name}</p>
-            <p>On: {new Date(post.date).getDate()}/{new Date(post.date).getMonth()}/{new Date(post.date).getFullYear()} at: {post.time}</p>
-          </Card>
-          )
-        })
-       }
-      </Space>
-    </div>
-  </>
+                >
+                  <p>Description</p>
+                  <p>By: {post?.userId.name}</p>
+                  <p>On: {new Date(post.date).getDate()}/{new Date(post.date).getMonth()}/{new Date(post.date).getFullYear()} at: {post.time}</p>
+                </Card>
+              )
+            })
+          }
+        </Space>
+      </div>
+    </>
 
 
   )
