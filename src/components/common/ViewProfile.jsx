eb4da4a87c5f7d25ff8e0803a9788cd28c417b26
AkashBawa <bawa2027@gmail.com>
2023-11-17 16:42:55 -0800

    view profile, post, dashboard

---
+++ src/components/common/ViewProfile.jsx
@@ -0,0 +1,23 @@
+const ViewProfile = ( {user }) => {
+  return (
+    <div id="viewProfile">
+        <h1>{user.name}</h1>
+        <div className="topBar">
+            <div className="left">
+                <img src={user.profilePhoto}/>
+            </div>
+            <div className="about">
+                I am a student who want to help our society.
+            </div>
+        </div>
+        <div className="otherDetails">
+            <p> <b>Gender</b> : {user.gender} </p>
+        </div>
+        <div>
+
+        </div>
+    </div>
+  )
+}
+
+export default ViewProfile
